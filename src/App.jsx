74b1283c6375087168303bd7a3ac61bc19259c4c
Iktan Strategies <antonio.sanchez@iktanst.com>
2022-06-28 23:12:02 -0500

    Actualizacion del enritamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+/* import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; */
 import "./App.scss";
+import Admin from "./pages/Admin";
+import SignIn from "./pages/Admin/SignIn";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
+    <div>
+      <h1>Estamos en App</h1>
+      <Admin />
+      <SignIn />
+      <Home />
+      <Contact />
+    </div>
+    /* 
     <Router>
       <div className="app">
         <h1>Sistema de rutas basico</h1>
@@ -19,9 +31,10 @@ function App() {
         </Routes>
       </div>
     </Router>
+     */
   );
 }
-
+/* 
 function Home() {
   return <h2>Estamos en el componente Home</h2>;
 }
@@ -37,5 +50,5 @@ function Users() {
 function Error404() {
   return <h2>Error 404...</h2>;
 }
-
+ */
 export default App;
